2f41995498c1def3b0650d04c04021cb7ed22746
FenrLabs <fenrir.git@gmail.com>
2024-06-26 13:48:46 +0200

    feat: set tool numbers
    sets now toolnumbers on created tools which are not been used.

--- NX2206/NET/ToolCreator/MVVM/ViewModels/MainViewModel.cs
+++ NX2206/NET/ToolCreator/MVVM/ViewModels/MainViewModel.cs
@@ -3,9 +3,11 @@ using NXTools.Drilling;
 using NXTools.General;
 using NXTools.Milling;
 using NXWrapper;
+using NXWrapper.Enumerations.CAM;
 using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Linq;
 using System.Windows.Input;
 
 namespace ToolCreator.MVVM.ViewModels {
@@ -13,6 +15,7 @@ namespace ToolCreator.MVVM.ViewModels {
 
         public List<ToolEntry> Tools { get; private set; }
         public ICommand CreateCommand { get; }
+        List<int> _settedToolNumbers, _availableNums = new List<int>();
 
         /// <summary>
         /// Erstellt Werkzeuge und löst das <see cref="OnRequestClose"/> Event aus.
@@ -34,6 +37,10 @@ namespace ToolCreator.MVVM.ViewModels {
         public MainViewModel() {
             Tools = GetTools();
             CreateCommand = new RelayCommand<object>(CreateCommandPressed);
+            _settedToolNumbers = GetAllSetToolNumbers();
+
+            for (int i = 1; i < 61; i++)
+                _availableNums.Add(i);
         }
 
         /// <summary>
@@ -43,8 +50,15 @@ namespace ToolCreator.MVVM.ViewModels {
             NX.SetUndoMark("Werkzeuge erstellen");
 
             foreach (var entry in Tools) {
+                // Quick & Dirty Werkzeugnummern zuweisen
+                // TODO - existierendem Werkzeug gleiche Nummer zuweisen
+                int num = _availableNums.Except(_settedToolNumbers).Min();
+                _settedToolNumbers.Add(num);
+
                 if (entry.Create) {
                     BaseTool tool;
+                    entry.Number = num;
+
                     switch (entry.Type) {
                         case ToolSubTypes.Mill:
                             tool = new EndMill(entry, Startup.ToolPocket);
@@ -127,6 +141,24 @@ namespace ToolCreator.MVVM.ViewModels {
 
             tools.Sort((x, y) => x.Name.CompareTo(y.Name));
             return tools;
+        }
+
+        /// <summary>
+        /// Gibt alle benutzten Werkzeugnummer zurück.
+        /// </summary>
+        /// <returns><![CDATA[List<int>]]></returns>
+        List<int> GetAllSetToolNumbers() {
+            List<int> toolNumbers = new List<int>();
+            foreach (var element in NX.CAM.MachineRoot.GetMembers()) {
+                var type = NX.GetNXType(element);
+                if (type.Type == (int)MachineTypes.TOOL) {
+                    var toolBuilder = NX.CAM.Collection.CreateTToolBuilder(element);
+                    toolNumbers.Add(toolBuilder.TlNumberBuilder.Value);
+                    toolBuilder.Destroy();
+                }
+            }
+
+            return toolNumbers;
         }
     }
 }
